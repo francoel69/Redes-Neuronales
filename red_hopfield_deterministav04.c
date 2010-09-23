1cce17bccb03a051a69c4154dbdfa1cab811fc92
Francusa Rodriguez <francoel69@gmail.com>
2010-09-23 12:01:46 -0300

    v04

---
+++ red_hopfield_deterministav04.c
@@ -0,0 +1,155 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+#include "ran2.h"
+#include "matrix.h"
+
+#define MAX_ITER 10
+#define N 832	//Cantidad de neuronas;
+#define PMAX (N/2)
+#define FIRING 1
+#define NOT_FIRING -1
+
+float uniform_gen(void);
+matrix memories(matrix xi, int p);
+// void synapses(matrix w, const matrix xi, int P);
+matrix calcular_m(const matrix s, const matrix xi, matrix m, int p);
+int firing(void);
+matrix initialize(matrix s, const matrix xi);
+int compare_vect(const matrix s, const matrix saux);
+int neuron_d(const matrix xi, const matrix m, int i, int p);
+
+int main(void){
+  matrix m, xi, s, saux;
+  int i= 0, j= 0, med= 0, p= 0, k= 0, m_medio= 0, mu= 0;
+
+//   w= create_matrix(N, N);
+  s= create_matrix(N, 1);
+  saux= create_matrix(N, 1);
+
+  for(p=4;p<=PMAX;p+=4){
+    m= create_matrix(p, 1);
+    xi= create_matrix(N, p);
+  
+    m_medio= 0;
+    for(k=0;k<MAX_ITER;k++){
+      xi= memories(xi, p);
+      initialize(s, xi);
+      m= calcular_m(s, xi, m, p);
+      while(compare_vect(s, saux)){
+	copy_matrix(s,saux);
+
+	for(i=0;i<N;i++){
+	  s= set(i, 0, neuron_d(xi,m,i,p), s);
+	  for(mu=0;mu<p;mu++){
+	    m= set(mu,0,get(i,mu,xi)*(get(i,0,s)-get(i,0,saux)),m);
+	  }
+	}
+      }
+      med= 0;
+      for(j=0;j<N;j++){
+	med += get(j,0,s)*get(j,0,xi);
+      }
+      m_medio += abs(med);
+    }
+    printf("%f\t%f\n", ((float)p)/N, ((float)m_medio)/(N*k));
+
+    destroy_matrix(xi);
+    destroy_matrix(m);
+  }
+//     destroy_matrix(w);
+    destroy_matrix(s);
+    destroy_matrix(saux);
+
+  return 0;
+}
+
+/* Generador de numeros aleatorios entre 0 y 1. */
+float uniform_gen(void){
+  return ran2(&seed);
+}
+
+/* Devuelve 1 o -1 aleatoriamente, con igual probabilidad. */
+int firing(void){
+  float f = uniform_gen();
+  return f>=0.5 ? FIRING : NOT_FIRING;
+}
+
+/* Rellena la matriz xi con 1 o -1 aleatoriamente. */
+matrix memories(matrix xi, int p){
+  int m=0, j=0;
+  for(j=0;j<N;j++){
+    for(m=0;m<p;m++){
+      xi = set(j, m, firing(), xi);
+    }
+  }
+  return xi;
+}
+
+/* Calcula la matriz sinaptica. */
+/*
+void synapses(matrix w, const matrix xi, int p){
+  int i= 0, j= 0, mu= 0, a= 0;
+
+  for(i=0;i<N;i++){
+    w= set(i,i,0,w);
+    for(j=0;j<i;j++){
+      a= 0;
+      for(mu=0;mu<p;mu++){
+        a+= get(i,mu,xi)*get(j,mu,xi);
+      }
+      w= set(i,j,a,w);
+      w= set(j,i,a,w);
+    }
+  }
+}*/
+
+matrix calcular_m(const matrix s, const matrix xi, matrix m, int p){
+  int mu= 0, j= 0, sum= 0;
+
+  for(mu=0;mu<p;mu++){
+    sum= 0;
+    for(j=0;j<N;j++){
+      sum += get(j,0,s)*get(j,mu,xi);
+    }
+    m= set(mu,0,sum,m);
+  }
+  return m;
+}
+
+/* Valor inicial del S. */
+matrix initialize(matrix s, const matrix xi){
+  int i= 0;
+
+  for(i=0;i<N;i++){
+    if(uniform_gen() < .85){
+      s= set(i, 0, get(i, 0, xi), s);
+    }else{
+      s= set(i, 0, -get(i, 0, xi), s);
+    }
+  }
+  return s;
+}
+
+/* Devuelve 0 si son iguales. */
+int compare_vect(const matrix s, const matrix saux){
+  int i= 0;
+
+  while(i<N && (get(i,0,s) == get(i,0,saux))){
+      i++;
+  }
+  return !(i==N);
+}
+
+/* Deterministica. */
+int neuron_d(const matrix xi, const matrix m, int i, int p){
+    float h = 0.;
+    int mu= 0;
+
+    for (mu=0; mu<p; mu++){
+        h += get(i,mu,xi) * get(mu,0,m);
+    }
+    return h < 0. ? NOT_FIRING : FIRING;
+}
