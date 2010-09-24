49497ddb5e430ef32b0c2d5c9c957c9e8ddf91d6
Francusa Rodriguez <francoel69@gmail.com>
2010-09-24 12:40:23 -0300

    Los cambios que te mande por mail

--- red_hopfield_deterministav04.c
+++ red_hopfield_deterministav04.c
@@ -6,8 +6,8 @@
 #include "ran2.h"
 #include "matrix.h"
 
-#define MAX_ITER 30
-#define N 832	//Cantidad de neuronas;
+#define MAX_ITER 10
+#define N 832 //Cantidad de neuronas;
 #define PMAX (N/2)
 #define FIRING 1
 #define NOT_FIRING -1
@@ -18,26 +18,26 @@ matrix memories(matrix xi, int p);
 matrix calcular_m(const matrix s, const matrix xi, matrix m, int p);
 matrix actualizar_m(int dif, int i, const matrix xi, matrix m, int p);
 int firing(void);
-matrix initialize(matrix s, const matrix xi);
+matrix initialize(matrix s, const matrix xi, int mu);
 int compare_vect(const matrix s, const matrix saux);
 int neuron_d(const matrix xi, const matrix m, const matrix s,int i, int p);
 
 int main(void){
   matrix m, xi, s, saux;
-  int i= 0, p= 0, k= 0, m_medio= 0, dif= 0;
+  int i= 0, p= 0, k= 0, m_medio= 0, dif= 0, min= 0;
+  float iter= 1.;
 
-//   w= create_matrix(N, N);
   s= create_matrix(N, 1);
   saux= create_matrix(N, 1);
+  m= create_matrix(PMAX, 1);
+  xi= create_matrix(N, PMAX);
 
-  for(p=40;p<=PMAX;p+=4){
-    m= create_matrix(p, 1);
-    xi= create_matrix(N, p);
-  
+  for(p=4;p<=PMAX;p+=4){
     m_medio= 0;
-    for(k=0;k<MAX_ITER;k++){
-      xi= memories(xi, p);
-      initialize(s, xi);
+    xi= memories(xi, p);
+    min= iter>MAX_ITER ? MAX_ITER : (int)iter;
+    for(k=0;k<min;k++){
+      initialize(s, xi, k%p);
       m= calcular_m(s, xi, m, p);
       while(compare_vect(s, saux)){
 	copy_matrix(s,saux);
@@ -48,16 +48,16 @@ int main(void){
 	  m= actualizar_m(dif, i, xi, m, p);
 	}
       }
-      m_medio += abs(get(0,0, m));
+      m_medio += abs(get(k%p,0, m));
     }
-    printf("%f\t%f\n", ((float)p)/N, ((float)m_medio)/(N*MAX_ITER));
+    iter *= 1.1;
+    printf("%f\t%f\n", ((float)p)/N, ((float)m_medio)/(N*min));
 
-    destroy_matrix(xi);
-    destroy_matrix(m);
   }
-//     destroy_matrix(w);
-    destroy_matrix(s);
-    destroy_matrix(saux);
+  destroy_matrix(s);
+  destroy_matrix(saux);
+  destroy_matrix(xi);
+  destroy_matrix(m);
 
   return 0;
 }
@@ -75,10 +75,10 @@ int firing(void){
 
 /* Rellena la matriz xi con 1 o -1 aleatoriamente. */
 matrix memories(matrix xi, int p){
-  int m=0, j=0;
+  int mu=0, j=0;
   for(j=0;j<N;j++){
-    for(m=0;m<p;m++){
-      xi = set(j, m, firing(), xi);
+    for(mu=0;mu<p;mu++){
+      xi = set(j, mu, firing(), xi);
     }
   }
   return xi;
@@ -125,14 +125,14 @@ matrix actualizar_m(int dif, int i, const matrix xi, matrix m, int p){
 }
 
 /* Valor inicial del S. */
-matrix initialize(matrix s, const matrix xi){
+matrix initialize(matrix s, const matrix xi, int mu){
   int i= 0;
 
   for(i=0;i<N;i++){
     if(uniform_gen() < 1.){
-      s= set(i, 0, get(i, 0, xi), s);
+      s= set(i, 0, get(i, mu, xi), s);
     }else{
-      s= set(i, 0, -get(i, 0, xi), s);
+      s= set(i, 0, -get(i, mu, xi), s);
     }
   }
   return s;
@@ -150,12 +150,12 @@ int compare_vect(const matrix s, const matrix saux){
 
 /* Deterministica. */
 int neuron_d(const matrix xi, const matrix m, const matrix s,int i, int p){
-    int h = 0.;
+    int h = 0;
     int mu= 0;
 
     for (mu=0; mu<p; mu++){
         h += get(i,mu,xi) * get(mu,0,m);
     }
     h = h-p*get(i,0,s);
-    return h < 0. ? NOT_FIRING : FIRING;
+    return h < 0 ? NOT_FIRING : FIRING;
 }
